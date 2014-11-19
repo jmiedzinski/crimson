56af4257f6681e782d6cec3d3bfb1209b2176547
Jeremiasz Miedzinski <jmiedzinski@gmail.com>
2014-11-19 16:44:01 +0100

    lights cd

--- core/src/zbk/fun/crimson/utils/LightsManager.java
+++ core/src/zbk/fun/crimson/utils/LightsManager.java
@@ -1,8 +1,14 @@
 package zbk.fun.crimson.utils;
 
+import box2dLight.RayHandler;
+
 public class LightsManager {
 
 	private static LightsManager instance;
+	
+	RayHandler rayHandler;
+	
+	pr
 	
 	private LightsManager() {
 		
